ce0c391afbfdcbbf6178b2740eb8c75ddd4eeff1
Wiris Wernek <wiriswernek@gmail.com>
2024-03-19 22:07:26 -0300

    feat: atualiza para angular 17 e adiciona firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
@@ -9,18 +14,22 @@ import { LoginComponent } from './views/login/login.component';
 import { TicketsComponent } from './views/tickets/tickets.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    TicketsComponent,
-    NavbarComponent,
-    FormTicketComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		TicketsComponent,
+		NavbarComponent,
+		FormTicketComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+		provideAuth(() => getAuth()),
+		provideFirestore(() => getFirestore()),
+		provideDatabase(() => getDatabase()),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
